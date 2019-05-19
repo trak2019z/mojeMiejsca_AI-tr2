8f57098317d681fc4df3fc9a9dcb0ef49b937a0b
Zbigniew Wnęk <{ID}+{username}@users.noreply.github.com>
2019-05-19 19:13:49 +0200

    Skonfigurowanie bazy danych na potrzeby aplikacji i modeli. Baza PostgreSQL działa na hoście lokalnym i otwarta jest na porcie 5432.

--- Moje ulubione miejsca/config/db.php
+++ Moje ulubione miejsca/config/db.php
@@ -2,13 +2,14 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=127.0.0.1;port=5432;dbname=ai',          
+    'username' => 'u_ai', 
+    'password' => ('m@ster007'),
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
-];
+]; 
+ 
