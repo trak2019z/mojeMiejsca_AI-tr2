13e926e5badb8cf8aff607d19b10f7c21e036899
Zbigniew Wnęk <{ID}+{username}@users.noreply.github.com>
2019-05-19 19:32:56 +0200

    Zmiana modelu użytkownika: - w regułach (rules) dodano informacje pojawiające się podczas   logowania o polach pustych, itd. - w etykietach pól spolszczono nazwy pól - dodano do klasy wymaganą implementację klasy IdentityInterface   potrzebną do przeprowadzania procesu logowania, zgodnie z   dokumentacją zawartą na stronie: https://www.yiiframework.com/doc/guide/2.0/en/security-authentication - dodano wirtualne funkcje konieczne do obsługi procesu logowania.

--- Moje ulubione miejsca/models/Uzytkownik.php
+++ Moje ulubione miejsca/models/Uzytkownik.php
@@ -2,7 +2,8 @@
 
 namespace app\models;
 
-use Yii;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
 
 /**
  * This is the model class for table "uzytkownik".
@@ -16,7 +17,7 @@ use Yii;
  * @property bool $ban
  * @property string $regcode
  */
-class Uzytkownik extends \yii\db\ActiveRecord
+class Uzytkownik extends ActiveRecord implements IdentityInterface
 {
     /**
      * {@inheritdoc}
@@ -32,13 +33,16 @@ class Uzytkownik extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
+            [['username', 'password', 'email', 'created_on', 'ban','email2','password2'], 'required','message'=>'Pole nie może być puste'],
             [['created_on', 'last_login'], 'safe'],
             [['ban'], 'boolean'],
-            [['username', 'password'], 'string', 'max' => 50],
-            [['email'], 'string', 'max' => 255],
-            [['regcode'], 'string', 'max' => 32],
-            [['email'], 'unique'],
-            [['username'], 'unique'],
+            [['username', 'password','password2','regcode'], 'string', 'max' => 50],
+            [['email','email2'], 'string', 'max' => 255],
+            [['email','email2'],'email','message'=>'Podaj prawidłowy adres e-mail.'],
+            [['email','regcode','username'], 'unique'],
+            ['email2','compare','compareAttribute'=>'email','operator'=>'===','message'=>'Pola adresu e-mail muszą być takie same'],
+            ['password2','compare','compareAttribute'=>'password','operator'=>'===','message'=>'Hasła muszą być takie same'],
+            ['verifyCode', 'captcha','message'=>'Podaj kod weryfikacyjny'],
         ];
     }
 
@@ -48,15 +52,48 @@ class Uzytkownik extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'user_id' => 'User ID',
-            'username' => 'Username',
-            'password' => 'Password',
-            'email' => 'Email',
-            'created_on' => 'Created On',
-            'last_login' => 'Last Login',
-            'ban' => 'Ban',
-            'regcode' => 'Regcode',
+            'user_id' => 'Numer ID użytkownika',
+            'username' => 'Nazwa użytkownika',
+            'password' => 'Hasło',
+            'password2'=>'Powtórz hasło',
+            'email' => 'Adres e-mail',
+            'email2'=>'Powtórz adres e-mail',
+            'created_on' => 'Data i godzina utworzenia konta',
+            'last_login' => 'Data i godzina ostatniego logowania',
+            'ban' => 'Zablokowane',
+            'verifyCode'=>'Kod weryfikacyjny',
+            'regcode'=>'Kod aktywacyjny'
         ];
+    }
+
+    public function getAuthKey(): string {
+        return $this->user_id;
+    }
+
+    public function getId() {
+        return $this->user_id;
+    }
+
+    public function validateAuthKey($authKey): bool {
+        return $this->user_id === $authKey;
+    }
+
+    public static function findIdentity($id): \yii\web\IdentityInterface {
+        return self::findOne($id);
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null): \yii\web\IdentityInterface {
+        throw new \yii\base\NotSupportedException;
+    }
+    
+    public function validatePassword($password)
+    {
+        return $this->password === md5($password);
+    }
+    
+    public static function findByUsername($username)
+    {
+        return self::findOne(['username'=>$username]);
     }
     
 }
