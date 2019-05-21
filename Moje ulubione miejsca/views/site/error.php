82142a4ba0317038eee596c4a7bf18915ef8a1e0
Zbigniew Wnęk <{ID}+{username}@users.noreply.github.com>
2019-05-21 15:49:29 +0200

    Zamiana strony błędów.

--- Moje ulubione miejsca/views/site/error.php
+++ Moje ulubione miejsca/views/site/error.php
@@ -7,21 +7,14 @@
 
 use yii\helpers\Html;
 
-$this->title = $name;
+$this->title ='Błąd!';
 ?>
 <div class="site-error">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+        <?= nl2br(Html::encode("Strona nie istnieje. Spróbuj ponownie później lub skontaktuj się z administratorem strony.")) ?>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
 
 </div>
