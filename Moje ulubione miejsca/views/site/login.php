a5f26f56dc5e84dfae31b1f1098b730fb4a69081
Zbigniew Wnęk <{ID}+{username}@users.noreply.github.com>
2019-05-21 13:04:10 +0200

    Zmiana wyglądu i funkcjonalności widoku logowania m.in. dodanie możliwości rejestracji użytkowników.

--- Moje ulubione miejsca/views/site/login.php
+++ Moje ulubione miejsca/views/site/login.php
@@ -6,42 +6,46 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use yii\helpers\Url;
 
-$this->title = 'Login';
+$this->title = 'Logowanie';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="panel panel-default col-lg-6 col-lg-offset-3">
+        <div class="panel-heading">Logowanie</div>
+            <div class="panel-body">
 
-    <p>Please fill out the following fields to login:</p>
+                <?php $form = ActiveForm::begin([
+                    'id' => 'login-form',
+                    'layout' => 'horizontal',
 
-    <?php $form = ActiveForm::begin([
-        'id' => 'login-form',
-        'layout' => 'horizontal',
-        'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-1 control-label'],
-        ],
-    ]); ?>
+                    'fieldConfig' => [
+                        'template' => "{label}\n<div class=\"col-lg-4\">{input}</div>\n<div class=\"col-lg-5\">{error}</div>",
+                        'labelOptions' => ['class' => 'col-lg-3 control-label'],
+                    ],
+                    ]); 
+                ?>
 
-        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label('Użytkownik') ?>
+                <?= $form->field($model, 'password')->passwordInput()->label('Hasło') ?>
 
-        <?= $form->field($model, 'password')->passwordInput() ?>
+                <div class="form-group">
+                <div class="col-lg-offset-5 col-lg-11">
+                    <?= Html::submitButton('Zaloguj', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                </div>
+                </div>
 
-        <?= $form->field($model, 'rememberMe')->checkbox([
-            'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-        ]) ?>
-
-        <div class="form-group">
-            <div class="col-lg-offset-1 col-lg-11">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            <?php ActiveForm::end(); ?>
+                
+            
             </div>
-        </div>
-
-    <?php ActiveForm::end(); ?>
-
-    <div class="col-lg-offset-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
     </div>
+    
+    <div class="panel col-lg-6 col-lg-offset-3">
+        <?= Html::tag('p','Nie jesteś zarejestrowanym użytkownikiem?',['class'=>'col-lg-7']) ?>
+        <?= Html::a('Zarejestruj się', Url::to(['uzytkownik/register']),['class'=>'col-lg-3']) ?>
+    </div>
+    
+    
 </div>
