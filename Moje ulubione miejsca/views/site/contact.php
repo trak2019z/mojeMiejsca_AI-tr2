22d72201ca78463c947ef07b61ca36af6df38d67
Zbigniew Wnęk <{ID}+{username}@users.noreply.github.com>
2019-05-21 14:01:04 +0200

    Zmiana wyglądu i funkcjonowania formularza kontaktowego zarówno dla użytkowników zalogowanych jak i niezalgowanych.

--- Moje ulubione miejsca/views/site/contact.php
+++ Moje ulubione miejsca/views/site/contact.php
@@ -8,55 +8,48 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use yii\captcha\Captcha;
 
-$this->title = 'Contact';
+$this->title = 'Kontakt';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-contact">
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="site-contact col-lg-offset-3 col-lg-8">
 
     <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
 
-        <div class="alert alert-success">
-            Thank you for contacting us. We will respond to you as soon as possible.
+        <div class="alert alert-success col-lg-6">
+            Dziękuję za wysłanie wiadomości. Postaram się odpowiedzieć najszybciej
+            jak to będzie możliwe.
         </div>
-
-        <p>
-            Note that if you turn on the Yii debugger, you should be able
-            to view the mail message on the mail panel of the debugger.
-            <?php if (Yii::$app->mailer->useFileTransport): ?>
-                Because the application is in development mode, the email is not sent but saved as
-                a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
-                Please configure the <code>useFileTransport</code> property of the <code>mail</code>
-                application component to be false to enable email sending.
-            <?php endif; ?>
-        </p>
 
     <?php else: ?>
 
         <p>
-            If you have business inquiries or other questions, please fill out the following form to contact us.
-            Thank you.
+            Jeśli masz jakieś pytania, uzupełnij formularz i wyślij. Dziękuję.
         </p>
 
         <div class="row">
-            <div class="col-lg-5">
+            <div class="col-lg-8">
 
-                <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
+                <?php $form = ActiveForm::begin(['id' => 'contact-form']);
 
-                    <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
-
-                    <?= $form->field($model, 'email') ?>
-
-                    <?= $form->field($model, 'subject') ?>
-
-                    <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
-
-                    <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                        'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                    ]) ?>
+                    if(Yii::$app->user->isGuest) {
+                    
+                        echo $form->field($model, 'name')->textInput(['autofocus' => true])->label('Imię');
+                        echo $form->field($model, 'email')->label('E-mail');
+                        echo $form->field($model, 'subject')->label('Tytuł');
+                        echo $form->field($model, 'body')->textarea(['rows' => 6])->label('Treść');
+                        echo $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+                            'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
+                        ]);
+                    } else {
+                        echo $form->field($model,'name')->hiddenInput(['value'=> Yii::$app->user->identity->username])->label(false);
+                        echo $form->field($model,'email')->hiddenInput(['value'=> Yii::$app->user->identity->email])->label(false);
+                        echo $form->field($model, 'subject')->label('Tytuł');
+                        echo $form->field($model, 'body')->textarea(['rows' => 6])->label('Treść');
+                    }
+                    ?>
 
                     <div class="form-group">
-                        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+                        <?= Html::submitButton('Wyślij', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
                     </div>
 
                 <?php ActiveForm::end(); ?>
